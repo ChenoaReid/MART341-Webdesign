7ef28c41a7ac9d7790b384bc9495e393cd6838da
Chenoa Reid-Croyle <144070674+ChenoaReid@users.noreply.github.com>
2023-12-14 21:31:04 -0700

    update

--- assignment_14/js/main.js
+++ assignment_14/js/main.js
@@ -2,8 +2,54 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var cty = c.getContext("2d");
 
-const image = new Image();
-image.src = "C:\Users\cnoaa\Documents\GitHub\MART341-Webdesign\assignment_13\images\IMG_0481.PNG";
+// blue triangle
+ctx.beginPath();
+ctx.lineTo(145, 405);
+ctx.lineTo(195, 305);
+ctx.lineTo(245, 405);
+ctx.closePath();
+ctx.fillStyle = '#0F2F50';
+ctx.fill();
+ctx.stroke();
 
-image.addEventListener("load", () => ctx.drawImage(image, 20, 20));
-canvas.setAttribute("DBA Logo");
+
+// blue triangle 2
+ctx.beginPath();
+ctx.lineTo(200, 300);
+ctx.lineTo(250, 400);
+ctx.lineTo(300, 300);
+ctx.closePath();
+ctx.fillStyle = '#0F2F50';
+ctx.fill();
+ctx.stroke();
+
+
+//blue triangle 3
+ctx.beginPath();
+ctx.lineTo(255,405);
+ctx.lineTo(305,305);
+ctx.lineTo(355,405);
+ctx.closePath();
+ctx.fillStyle = '#0F2F50';
+ctx.fill();
+ctx.stroke();
+
+//blue triangle 4
+ctx.beginPath();
+ctx.lineTo(200,295);
+ctx.lineTo(250,195);
+ctx.lineTo(300,295);
+ctx.closePath();
+ctx.fillStyle = '#0F2F50';
+ctx.fill();
+ctx.stroke();
+
+//fill text
+ctx.font = '30pt Courier';
+ctx.strokeText('DICEBERG Ahead!', 100, 100);
+ctx.lineWidth = 5;
+
+//Author
+ctx.font = '15pt Courier';
+ctx.fillStyle = 'black';
+ctx.fillText('Making TTRPG Content', 100, 150);
